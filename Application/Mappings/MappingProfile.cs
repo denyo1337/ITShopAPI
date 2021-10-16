13fa9de23748bad89cfc1f1f73fdb6daa716ca6e
denyo1337 <143619@student.uwm.edu.pl>
2021-10-16 11:54:38 +0200

    Automapper cfg done

---
+++ Application/Mappings/MappingProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            ApplyMappingsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes().Where(x =>
+            typeof(IMap).IsAssignableFrom(x) && !x.IsInterface).ToList();
+            foreach (var item in types)
+            {
+                var instance = Activator.CreateInstance(item);
+                var methodInfo = item.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new object[] { this });
+            }
+        }
+    }
+}
