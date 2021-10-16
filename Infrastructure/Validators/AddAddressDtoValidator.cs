e44e036ff79ac0a18831f69f6005e6134ccecf03
denyo1337 <143619@student.uwm.edu.pl>
2021-10-16 22:01:25 +0200

    Added postalCode validation

---
+++ Infrastructure/Validators/AddAddressDtoValidator.cs
@@ -0,0 +1,39 @@
+﻿using Application.DTOs;
+using FluentValidation;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Validators
+{
+    public class AddAddressDtoValidator : AbstractValidator<AddressDto>
+    {
+        public AddAddressDtoValidator(ITShopDbContext dbcontext)
+        {
+            RuleFor(x => x.Street)
+                   .NotEmpty()
+                   .MaximumLength(200);
+            RuleFor(x => x.Towm)
+                .NotEmpty()
+                .MaximumLength(200);
+            RuleFor(x => x.PostalCode)
+                .Custom((value, context) =>
+                {
+                    int leftParse = 0;
+                    var parts = value.Split("-");
+                    var leftSide = int.TryParse(parts[0], out leftParse);
+                    var rightSide = int.TryParse(parts[1], out leftParse);
+                    if (!value.Contains("-") && parts[0].Length>2 && parts[1].Length>3 && leftSide && rightSide)
+                    {    
+                        context.AddFailure("Podany zły format kodu pocztowego");
+                    }
+                });
+            RuleFor(x => x.Country)
+                .NotEmpty()
+                .MaximumLength(200);
+        }
+    }
+}
