455a9e0f6d3a1e01d9a7669e10e4f04219ce82ef
denyo1337 <143619@student.uwm.edu.pl>
2021-10-16 11:55:11 +0200

    DbSets Done and cfg'ed

---
+++ Infrastructure/Data/ITShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ITShopDbContext : DbContext
+    {
+        public ITShopDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+    }
+}
