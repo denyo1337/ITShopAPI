238df173fb133da38dcef6faea5e3156ac56333a
denyo1337 <143619@student.uwm.edu.pl>
2021-10-16 11:54:54 +0200

    Entities for User and Roles done

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User : AuditableEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string NickName { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string SurrName { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string Nationality { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime? BirthDay { get; set; }
+        public virtual Role Role { get; set; }
+        public int RoleId { get; set; }
+
+    }
+}
