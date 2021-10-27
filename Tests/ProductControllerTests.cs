e71c141a2039e4774e55674aef62dddef4139215
denyo1337 <143619@student.uwm.edu.pl>
2021-10-27 21:38:49 +0200

    Moved client initialization to ctor

--- Tests/ProductControllerTests.cs
+++ Tests/ProductControllerTests.cs
@@ -7,22 +7,48 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using WebAPI;
 using System.Threading.Tasks;
 using FluentAssertions;
+using System.Net.Http;
+
 namespace Tests
 {
     public class ProductControllerTests
     {
-        [Fact]
-        public async Task GetAll_WithQueryParameters_ReturnsOkResultAsync()
+        private HttpClient _client;
+
+        public ProductControllerTests()
         {
-            //arrange
             var factory = new WebApplicationFactory<Startup>();
-            var client = factory.CreateClient();
+            _client = factory.CreateClient();
+        }
+
+        [Theory]
+        [InlineData(5, 1)]
+        [InlineData(10, 1)]
+        [InlineData(20,1)]
+        [InlineData(50,1)]
+        public async Task GetAll_WithQueryParameters_ReturnsOkResultAsync(int pageSize, int pageNumber)
+        {
+
 
             //act
-            var response = await client.GetAsync("/api/Product?PageSize=5&pageNumber=1");
+            var response = await _client.GetAsync($"/api/Product?PageSize={pageSize}&pageNumber={pageNumber}");
             //assert
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
 
+        }
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(16, 1)]
+        [InlineData(22, 1)]
+        [InlineData(12, 1)]
+        [InlineData(0,0)]
+        public async Task GetAll_WithInvalidQueryParameters_ReturnsBadRequestStatus(int pageSize, int pageNumber)
+        {
+
+            //act
+            var response = await _client.GetAsync($"/api/Product?PageSize={pageSize}&pageNumber={pageNumber}");
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
     }
 }
